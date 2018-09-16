78c4c3f85eda39a9384c78fd6360bd48c62af79c
YYTB <suchao.personal@gmail.com>
2018-09-16 13:16:59 +0800

    update

--- tourism_website/tourism_website/middlewares.py
+++ tourism_website/tourism_website/middlewares.py
@@ -59,54 +59,54 @@ class TourismWebsiteSpiderMiddleware(object):
         spider.logger.info('Spider opened: %s' % spider.name)
 
 
+# class TourismWebsiteDownloaderMiddleware(object):
+#     # Not all methods need to be defined. If a method is not defined,
+#     # scrapy acts as if the downloader middleware does not modify the
+#     # passed objects.
+#
+#     @classmethod
+#     def from_crawler(cls, crawler):
+#         # This method is used by Scrapy to create your spiders.
+#         s = cls()
+#         crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
+#         return s
+#
+#     def process_request(self, request, spider):
+#         # Called for each request that goes through the downloader
+#         # middleware.
+#
+#         # Must either:
+#         # - return None: continue processing this request
+#         # - or return a Response object
+#         # - or return a Request object
+#         # - or raise IgnoreRequest: process_exception() methods of
+#         #   installed downloader middleware will be called
+#         return None
+#
+#     def process_response(self, request, response, spider):
+#         # Called with the response returned from the downloader.
+#
+#         # Must either;
+#         # - return a Response object
+#         # - return a Request object
+#         # - or raise IgnoreRequest
+#         return response
+#
+#     def process_exception(self, request, exception, spider):
+#         # Called when a download handler or a process_request()
+#         # (from other downloader middleware) raises an exception.
+#
+#         # Must either:
+#         # - return None: continue processing this exception
+#         # - return a Response object: stops process_exception() chain
+#         # - return a Request object: stops process_exception() chain
+#         pass
+#
+#     def spider_opened(self, spider):
+#         spider.logger.info('Spider opened: %s' % spider.name)
+
+
 class TourismWebsiteDownloaderMiddleware(object):
-    # Not all methods need to be defined. If a method is not defined,
-    # scrapy acts as if the downloader middleware does not modify the
-    # passed objects.
-
-    @classmethod
-    def from_crawler(cls, crawler):
-        # This method is used by Scrapy to create your spiders.
-        s = cls()
-        crawler.signals.connect(s.spider_opened, signal=signals.spider_opened)
-        return s
-
-    def process_request(self, request, spider):
-        # Called for each request that goes through the downloader
-        # middleware.
-
-        # Must either:
-        # - return None: continue processing this request
-        # - or return a Response object
-        # - or return a Request object
-        # - or raise IgnoreRequest: process_exception() methods of
-        #   installed downloader middleware will be called
-        return None
-
-    def process_response(self, request, response, spider):
-        # Called with the response returned from the downloader.
-
-        # Must either;
-        # - return a Response object
-        # - return a Request object
-        # - or raise IgnoreRequest
-        return response
-
-    def process_exception(self, request, exception, spider):
-        # Called when a download handler or a process_request()
-        # (from other downloader middleware) raises an exception.
-
-        # Must either:
-        # - return None: continue processing this exception
-        # - return a Response object: stops process_exception() chain
-        # - return a Request object: stops process_exception() chain
-        pass
-
-    def spider_opened(self, spider):
-        spider.logger.info('Spider opened: %s' % spider.name)
-
-
-class UrlFilter(object):
     # 初始化过滤器（使用mongodb过滤）
     def __init__(self):
         self.settings = get_project_settings()
@@ -115,5 +115,5 @@ class UrlFilter(object):
         self.coll = self.db[self.settings['MONGO_COLL']]
 
     def process_request(self, request, spider):
-        if (self.bookColl.count({"url": request.url}) > 0):
-            return http.Response(url=request.url, body=None)
+        if self.coll.count({"url": request.url}) > 0:
+            return spider.Response(url=request.url, body=None)
